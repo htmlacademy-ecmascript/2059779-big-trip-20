1c300a815c9cce4248b735242f395f69bba57384
Vladimir Trofimov <i_am@palant.org>
2023-05-11 16:52:41 +0300

    Adds constants

--- src/utils.js
+++ src/utils.js
@@ -6,4 +6,4 @@ function getRandomNumber(min = 0, max = 1) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export { getRandomArrayElement, getRandomNumber, getRandomBoolean };
+export { getRandomArrayElement, getRandomNumber };
